ac60be093b2a6452ce23fb7326313c7e7833f9b3
asleave <asleave@gmail.com>
2025-07-27 13:56:08 +0800

    更新

--- database/config.py
+++ database/config.py
@@ -6,7 +6,7 @@ import os
 env_path = Path(__file__).resolve().parents[1] / ".env"
 load_dotenv(dotenv_path=env_path)
 
-MYSQL_HOST = os.getenv("MYSQL_HOST", "35.208.49.121")
+MYSQL_HOST = os.getenv("MYSQL_HOST", "35.208.106.174")
 MYSQL_PORT = int(os.getenv("MYSQL_PORT", 3306))
 MYSQL_ACCOUNT = os.getenv("MYSQL_ACCOUNT", "root")
 MYSQL_PASSWORD = os.getenv("MYSQL_PASSWORD", "zuJwix-6marby-nyrbov")
