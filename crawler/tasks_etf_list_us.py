1627a3363a3589c94c0d8f4126aed534145ba1fe
pei yu ji <abc0970870@gmail.com>
2025-09-06 21:56:32 +0800

    feat

--- crawler/tasks_etf_list_us.py
+++ crawler/tasks_etf_list_us.py
@@ -1,6 +1,9 @@
 import requests
 from bs4 import BeautifulSoup
 import pandas as pd
+import yfinance as yf
+from datetime import datetime
+
 from database.main import write_etfs_to_db
 from crawler.worker import app
 
@@ -26,9 +29,23 @@ def etf_list_us(crawler_url):
             name = name_tag.get_text(strip=True)
             region = "US"  # 手動補上國別
             currency = "USD"  # 手動補上幣別
-            etf_codes.append((code, name, region, currency))
+            try:
+                etf = yf.Ticker(code)
+                info = etf.info
+                expense_ratio = info.get("netExpenseRatio", None)
+                inception_ts = info.get("fundInceptionDate", None)
+                inception_date = (
+                datetime.fromtimestamp(inception_ts).strftime("%Y-%m-%d") if inception_ts else None
+                        )
+            except Exception as e:
+                        print(f"查詢 {code} 時發生錯誤: {e}")
+                        expense_ratio = None
+                        inception_date = None
 
+            etf_codes.append((code, name, region, currency, expense_ratio, inception_date))
+            
     # 將資料放入 DataFrame
-    etf_list_df = pd.DataFrame(etf_codes, columns=["etf_id", "etf_name", "region", "currency"])
+    etf_list_df = pd.DataFrame(etf_codes, columns=["etf_id", "etf_name", "region", "currency",
+                                               "expense_ratio","inception_date"])
     write_etfs_to_db(etf_list_df)
     return etf_list_df.to_dict(orient="records")
