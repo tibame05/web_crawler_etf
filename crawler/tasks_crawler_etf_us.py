faae7cce6c760f198f0e0b0ab0d05adee0b26ee5
pei yu ji <abc0970870@gmail.com>
2025-09-06 18:08:10 +0800

    refector

--- crawler/tasks_crawler_etf_us.py
+++ crawler/tasks_crawler_etf_us.py
@@ -1,62 +1,21 @@
 import pandas as pd
 import yfinance as yf
-from selenium import webdriver
-from selenium.webdriver.chrome.options import Options
-from bs4 import BeautifulSoup
-import time
-
-
-from selenium.webdriver.common.by import By
-from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
-
-
+import pandas_ta as ta
+import numpy as np
 from crawler.worker import app
+from database.main import write_etf_daily_price_to_db
 
 # 註冊 task, 有註冊的 task 才可以變成任務發送給 rabbitmq
 @app.task()
-def crawler_etf_us(url):
-
-
-
-    options = Options()
-    options.add_argument("--headless")
-    options.add_argument("--disable-gpu")
-    options.add_argument("--no-sandbox")
-    driver = webdriver.Chrome(options=options)
-    driver.get(url)
-
-    # 等待表格載入
-    WebDriverWait(driver, 15).until(
-        EC.presence_of_element_located((By.CSS_SELECTOR, "table tbody tr"))
-    )
-
-    html = driver.page_source
-    soup = BeautifulSoup(html, "html.parser")
-
-    etf_data = []
-
-    # 逐列抓取
-    rows = soup.select("table tbody tr")
-    for row in rows:
-        code_tag = row.select_one('a[href^="/symbols/"]')
-        name_tag = row.select_one("sup")
-        
-        if code_tag and name_tag:
-            code = code_tag.get_text(strip=True)
-            name = name_tag.get_text(strip=True)
-            etf_data.append((code, name))
-
-    driver.quit()
-
-    etf_codes = [code for code, _ in etf_data]
-        
+def crawler_etf_us(etf_list_df):
+    
+    tickers = [etf["etf_id"] for etf in etf_list_df]    
     start_date = '2015-05-01'
     end_date = pd.Timestamp.today().strftime('%Y-%m-%d')
 
     failed_tickers = []
-
-    for r in etf_codes:
+    all_etf_data = []
+    for r in tickers:
         print(f"正在下載：{r}")
         try:
             df = yf.download(r, start=start_date, end=end_date, auto_adjust=False)
@@ -79,10 +38,46 @@ def crawler_etf_us(url):
             continue
         df.columns = df.columns.droplevel(1)  # 把 'Price' 這層拿掉
 
+        # RSI (14) (相對強弱指標)
+        df["rsi"] = ta.rsi(df["close"], length=14)
+
+        # MA5、MA20（移動平均線）（也可以使用 df['close'].rolling(5).mean())）
+        df["ma5"] = ta.sma(df["close"], length=5)
+        df["ma20"] = ta.sma(df["close"], length=20)
+
+        # MACD（移動平均收斂背離指標）
+        macd = ta.macd(df["close"], fast=12, slow=26, signal=9)
+        df["macd_line"] = macd["MACD_12_26_9"]
+        df["macd_signal"] = macd["MACDs_12_26_9"]
+        df["macd_hist"] = macd["MACDh_12_26_9"]
+
+        # KD 指標（STOCH: 隨機震盪指標）
+        stoch = ta.stoch(df["high"], df["low"], df["close"], k=14, d=3, smooth_k=3)
+        df["pct_k"] = stoch["STOCHk_14_3_3"]
+        df["pct_d"] = stoch["STOCHd_14_3_3"]
+
+        # 增加該日報酬率與累積報酬指數
+        df['daily_return'] = df['adj_close'].pct_change()
+        df['cumulative_return'] = (1 + df['daily_return']).cumprod()
         df.insert(0, "etf_id", r)  # 新增一欄「etf_id」
- 
+        print (df)
         #df.columns = ["etf_id","date", "dividend_per_unit"]    # 調整欄位名稱
-        columns_order = ['etf_id', 'date', 'adj_close','close','high', 'low', 'open','volume']
+        columns_order = ['etf_id', 'date', 'adj_close','close','high', 'low', 'open','volume',
+                        'rsi', 'ma5', 'ma20', 'macd_line', 'macd_signal', 'macd_hist',
+                        'pct_k', 'pct_d', 'daily_return', 'cumulative_return']
         df = df[columns_order]
 
+        
+        df.replace([np.inf, -np.inf], np.nan, inplace=True)
+        df = df.replace([np.inf, -np.inf], np.nan)
+        #df = df.dropna(subset=[
+    #'rsi', 'ma5', 'ma20', 'macd_line', 'macd_signal', 'macd_hist',
+    #'pct_k', 'pct_d', 'daily_return', 'cumulative_return'])
+        all_etf_data.append(df)
+
+    # Step 4️⃣ 合併所有 ETF 成一張總表
+    if all_etf_data:
+        daily_price_df = pd.concat(all_etf_data, ignore_index=True)
+        daily_price_df = daily_price_df[columns_order]
+    write_etf_daily_price_to_db(daily_price_df)
     # return df
